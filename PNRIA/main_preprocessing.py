73bc60c63b060db2f634b879cbe666559c6f37e3
ElliotMaitre <elliot.maitre@gmail.com>
2024-10-17 05:07:52 -0400

    fixing conflicts

--- PNRIA/main_preprocessing.py
+++ PNRIA/main_preprocessing.py
@@ -6,29 +6,17 @@ from PNRIA.configs.config import load_yaml
 from PNRIA.utils.preprocessing import BasePreprocessing
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     use_case = "patches"
-=======
-    use_case = "mosaic"
->>>>>>> 2994ce1 (fixing dataset according to ML, synchro with modular-integration)
     config = load_yaml("/home/cloud-user/work/Toolbox/PNRIA/configs/config_preprocessing.yml")
     
     if use_case == "patches":
 
         # Load the model from the configuration
-<<<<<<< HEAD
         preprocessor = BasePreprocessing.from_config(config['preprocessing_patches'])
-=======
-        preprocessor = BasePreprocessing.from_config(config['preprocessing'])
->>>>>>> 2994ce1 (fixing dataset according to ML, synchro with modular-integration)
         preprocessor.create_folds()
         
     elif use_case == "mosaic":
         
         # Load the model from the configuration
-<<<<<<< HEAD
         preprocessor = BasePreprocessing.from_config(config['preprocessing_mosaics'])
-=======
-        preprocessor = BasePreprocessing.from_config(config['preprocessing'])
->>>>>>> 2994ce1 (fixing dataset according to ML, synchro with modular-integration)
         preprocessor.create_folds()
