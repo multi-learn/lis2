2802279b857074c3b4d305b30a626fd50015bf97
ElliotMaitre <elliot.maitre@gmail.com>
2025-02-13 09:53:21 -0500

    merge conflict + config_mosaic

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,12 @@
 from .distributed import get_rank, get_rank_num, is_main_gpu, synchronize
 from .normalizer import normalize_direct, normalize_histo, normalize_adapt_histo
 
-__all__ = ['get_rank', 'get_rank_num', 'is_main_gpu', 'synchronize', 'normalize_direct', 'normalize_histo',
-           'normalize_adapt_histo']
+__all__ = [
+    "get_rank",
+    "get_rank_num",
+    "is_main_gpu",
+    "synchronize",
+    "normalize_direct",
+    "normalize_histo",
+    "normalize_adapt_histo",
+]
